7f9ce587de4797b9bbd435885417ffa234c9f268
quangthanhphi <phithanh003@gmail.com>
2024-04-15 22:16:19 +0700

    trello color

--- src/components/AppBar/Menus/Profiles.jsx
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -33,7 +33,7 @@ function Profiles() {
           aria-expanded={open ? 'true' : undefined}
         >
           <Avatar
-            sx={{ width: 34, height: 34 }}
+            sx={{ width: 36, height: 36 }}
             alt="avatar"
             src=''
           />
