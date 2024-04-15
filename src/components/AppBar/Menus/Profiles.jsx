76c7c51575b75caea1a9c11043d17ee16a00a022
quangthanhphi <phithanh003@gmail.com>
2024-04-15 18:26:42 +0700

    Board Bar

--- src/components/AppBar/Menus/Profiles.jsx
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -33,7 +33,7 @@ function Profiles() {
           aria-expanded={open ? 'true' : undefined}
         >
           <Avatar
-            sx={{ width: 30, height: 30 }}
+            sx={{ width: 34, height: 34 }}
             alt="avatar"
             src=''
           />
