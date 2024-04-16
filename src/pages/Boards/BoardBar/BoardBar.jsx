3a78a0e75650f3fbcd83550f8a9c6a8b9ceb8308
quangthanhphi <phithanh003@gmail.com>
2024-04-16 22:21:58 +0700

    Mock Data and Order Algorithm

--- src/pages/Boards/BoardBar/BoardBar.jsx
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -10,6 +10,7 @@ import AvatarGroup from '@mui/material/AvatarGroup'
 import { Tooltip } from '@mui/material'
 import Button from '@mui/material/Button'
 import PersonAddIcon from '@mui/icons-material/PersonAdd'
+import {capitalizeFirstLetter} from '~/utils/formatters'
 
 const MENU_STYLES = {
   color: 'white',
@@ -25,7 +26,7 @@ const MENU_STYLES = {
   }
 }
 
-function BoardBar() {
+function BoardBar({board}) {
   return (
     <Box
       sx={{
@@ -44,13 +45,13 @@ function BoardBar() {
         <Chip
           sx={MENU_STYLES}
           icon={<DashboardIcon />}
-          label="MERN Stack Board"
+          label={board?.title}
           clickable
         />
         <Chip
           sx={MENU_STYLES}
           icon={<VpnLockIcon />}
-          label="Public/Private workspace"
+          label= {capitalizeFirstLetter(board?.type)}
           clickable
         />
         <Chip
